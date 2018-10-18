e315bfbbb563b97e46e5289959e559f9f0e53ef2
Junmo <yunedoopea610@gmail.com>
2018-10-18 17:16:24 +0900

    Add flexbox two attribute
    justifyStart: flex-start
    justifyEnd: flex-end

--- src/components/Units/UnitFlexBox.vue
+++ src/components/Units/UnitFlexBox.vue
@@ -11,18 +11,34 @@
 export default {
   name: 'UnitFlexBox',
   props: {
+    justifyStart: {
+      type: Boolean,
+      default: true,
+    },
     justifyCenter: {
+      type: Boolean,
+      default: false,
+    },
+    justifyEnd: {
       type: Boolean,
       default: false,
     },
   },
   computed: {
-    textAlign() {
-      return this.justifyCenter ? 'center' : '';
+    justifyContent() {
+      let value;
+      if (this.justifyEnd) {
+        value = 'flex-end';
+      } else if (this.justifyStart) {
+        value = 'center';
+      } else if (this.justifyLeft) {
+        value = 'flex-start';
+      }
+      return value;
     },
     styles() {
       return {
-        justifyContent: this.textAlign,
+        justifyContent: this.justifyContent,
       };
     },
   },
