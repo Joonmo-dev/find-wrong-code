5e6291d865b068b5817ae1def6fc53ecec0297f7
Junmo <yunedoopea610@gmail.com>
2018-10-18 15:52:57 +0900

    Start main page
    add
    - title
    - subtitle
    - two link buttons

---
+++ src/components/PageMain.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page-main">
+    <unit-container>
+      <unit-title align-center>
+        Finding Wrong Code
+      </unit-title>
+      <unit-title
+        :level="3"
+        align-center
+      >
+        언어 선택
+      </unit-title>
+      <unit-flex-box justify-center>
+        <unit-btn class="link-btn">C</unit-btn>
+        <unit-btn class="link-btn">Javascript</unit-btn>
+      </unit-flex-box>
+    </unit-container>
+  </div>
+</template>
+
+<script>
+import Units from './Units/Units';
+
+export default {
+  name: 'PageMain',
+  components: {
+    ...Units,
+  },
+};
+</script>
+
+<style scoped>
+.link-btn {
+  margin: 0 20px;
+}
+</style>
